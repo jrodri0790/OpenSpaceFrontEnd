675ab49add78d402c3797a208f3e1d2fb847963c
Yadira <ymarin@thoughtworks.com>
2019-08-27 09:57:10 -0500

    Loads tasks from database

--- src/components/open-space/OpenSpaceTalks.js
+++ src/components/open-space/OpenSpaceTalks.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Checkbox from '@material-ui/core/Checkbox'
-import talks from '../../data/talks'
+import axios from 'axios/index'
 
 class OpenSpaceTalks extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      talks: []
+    }
+    this.getTalks()
     this.updateButtonEnabledFlag = this.updateButtonEnabledFlag.bind(this)
     this.buildTalkCheckedStateName = this.buildTalkCheckedStateName.bind(this)
   }
 
   updateButtonEnabledFlag () {
     this.props.setValidityOfSelectedTalks(this.getCheckedTalks())
+  }
+
+  getTalks () {
+    const talksEndpointURI = 'http://localhost:5000/open-space/talks'
+    axios.get(talksEndpointURI)
+    .then((response) => {
+      if (response.status === 200) {
+        this.setState({talks: response.data})
+      }
+    })
+    .catch(() => {
+      const typeNotification = 'error'
+      this._showNotification('Oops ha ocurrido un error, inténtelo más tarde.', typeNotification)
+      this.setState({talks: []})
+    })
   }
 
   getCheckedTalks () {
@@ -34,23 +52,23 @@ class OpenSpaceTalks extends Component {
   }
 
   render() {
+    const { talks } = this.state
     const openSpaceTalks = new Map()
+
     talks.forEach(talk => {
       const disabled = this.getCheckedTalks().length >= 3 && !this.state[this.buildTalkCheckedStateName(talk.backendCode)]
-      if (talk.isTalk && !talk.isInformative) {
-        openSpaceTalks.set(talk.startDate,
-          <div key={this.buildTalkCheckedStateName(talk.backendCode)} className={disabled ? "open-space-talk disabled" : "open-space-talk"}>
-            <FormControlLabel disabled={disabled} className="open-space-title-talk"
-              control={
-                <Checkbox checked={this.state[this.buildTalkCheckedStateName(talk.backendCode)]}
-                          onChange={(event) => this.handleChange(event, this.buildTalkCheckedStateName(talk.backendCode))}
-                          value={talk.startDate} />
-              }
-              label={talk.title}
-            />
-          </div>
-        )
-      }
+      openSpaceTalks.set(talk.id,
+        <div key={this.buildTalkCheckedStateName(talk.id)} className={disabled ? "open-space-talk disabled" : "open-space-talk"}>
+          <FormControlLabel disabled={disabled} className="open-space-title-talk"
+            control={
+              <Checkbox checked={this.state[this.buildTalkCheckedStateName(talk.id)]}
+                        onChange={(event) => this.handleChange(event, this.buildTalkCheckedStateName(talk.id))}
+                        value={talk.id} />
+            }
+            label={talk.name}
+          />
+        </div>
+      )
     })
     return (
       <div className="open-space-talks-container">
